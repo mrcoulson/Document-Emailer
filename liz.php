7abeeed2d3414643dbd0ec5936fc54f0c07f6aa5
Jeremy Coulson <jeremycoulson@gmail.com>
2011-05-09 23:14:37 -0400

    new branch using swift mailer

--- liz.php
+++ liz.php
@@ -4,6 +4,8 @@
 // error_reporting(E_ALL);
 // ini_set('display_errors','On');
 
+require_once '/home/jcoulson/jcoulson.com/document-emailer/swift/lib/swift_required.php'; // Include Swift Mailer
+
 // Get variables.
 $textFile = "liz.txt";
 $strToName = $_POST["txtName"];
@@ -12,66 +14,51 @@ $strIP = $_SERVER['REMOTE_ADDR'];
 $strDateTime = date("Y-n-j H:i:s", time());
 
 // Connect to db.
-$link = mysql_connect ("localhost", "pdfdownload", "pdfdownload") or die ("Error: " . mysql_error());
+$link = mysql_connect ("localhost", "user", "pass") or die ("Error: " . mysql_error());
 mysql_select_db("pdfdownload");
 // Perform query.
 $query = "INSERT INTO requests (name, email, ip, request_datetime) VALUES ('" . $strToName . "', '" . $strToEmail . "', '" . $strIP . "', '" . $strDateTime . "')";
 mysql_query($query) or die ("Error updating database.");
 // Close db connection.
-mysql_close ($link) ;
+mysql_close ($link);
 
 // Display link.
 echo "Added.<br /><a href='liz.html'>Back</a>";
 
 
-// send email...
+// Send email.
 
 $strTo = "$strToName <$strToEmail>"; // this uses the submitted txtName and txtEmail from the requestor
 $strFromName = "Webmaster"; // edit this to your specified name
 $strFromEmail = "webmastr@co.frederick.va.us"; // edit this to your specified email address
 $strSubject = "super awesome pdf attached!"; // edit this to your specified subject text
 $strMessage = "Here is the file you requested."; // edit this to you specified message text
-
-// echo $_SERVER['DOCUMENT_ROOT']; // uncomment this line to determine where $_SERVER['DOCUMENT_ROOT'] points to.
-// edit your_path_here so that it completes the local server path to the file attachment
-$fileattpath = $_SERVER['DOCUMENT_ROOT']."/webmaster/";
-
-$fileatttype = "application/pdf"; // edit this to your specified attached file type
 $fileattname = "lizpdf.pdf"; // edit this to your specified filename
 
-// call mail_attachment function
-mail_attachment($fileattname, $fileattpath, $fileatttype, $strTo, $strFromEmail, $strFromName, $strFromEmail, $strSubject, $strMessage);
+//Create the transport with your SMTP settings (Gmail here as example)
+$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+  ->setUsername('user@gmail.com')
+  ->setPassword('pass')
+  ;
 
-function mail_attachment($filename, $path, $filetype, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
-    $file = $path.$filename;
-    $file_size = filesize($file);
-    $handle = fopen($file, "rb");
-    $content = fread($handle, $file_size);
-    fclose($handle);
-    $content = chunk_split(base64_encode($content));
-    $uid = md5(uniqid(time()));
-    $name = basename($file);
-    $header = "From: ".$from_name." <".$from_mail.">\r\n";
-    $header .= "Reply-To: ".$replyto."\r\n";
-    $header .= "MIME-Version: 1.0\r\n";
-    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
-    $header .= "This is a multi-part message in MIME format.\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
-    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
-    $header .= $message."\r\n\r\n";
-    $header .= "--".$uid."\r\n";
-    $header .= "Content-Type: ". $filetype ."; name=\"".$filename."\"\r\n";
-    $header .= "Content-Transfer-Encoding: base64\r\n";
-    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
-    $header .= $content."\r\n\r\n";
-    $header .= "--".$uid."--";
-    if (mail($mailto, $subject, "", $header)) {
-        echo "mail send ... OK"; // or use booleans here
-    } else {
-        echo "mail send ... ERROR!";
-    }
-}
+//Create the mailer using your created transport
+$mailer = Swift_Mailer::newInstance($transport);
 
+//Create the message
+$message = Swift_Message::newInstance()
+	//Give the message a subject
+	->setSubject($strSubject)
+	//Set the From address with an associative array
+  	->setFrom(array($strFromEmail => $strFromName))
+	//Set the To addresses with an associative array
+  	->setTo(array($strToEmail => $strTo))
+	//Give it a body
+ 	->setBody($strMessage)
+	//Optionally add any attachments
+  	->attach(Swift_Attachment::fromPath($fileattname))
+	;
+
+//Send the message
+$result = $mailer->send($message);
 
 ?>
