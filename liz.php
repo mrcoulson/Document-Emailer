a1817dc1fd635d9ac2692d6e8ce131b74da4bafd
Jeremy Coulson <jeremycoulson@gmail.com>
2011-05-13 15:33:56 -0400

    added confirmation of sent email

--- liz.php
+++ liz.php
@@ -1,10 +1,16 @@
 <?php
 
 // Uncomment to see detailed error messages.
-// error_reporting(E_ALL);
-// ini_set('display_errors','On');
+error_reporting(E_ALL);
+ini_set('display_errors','On');
 
-require_once '/home/jcoulson/jcoulson.com/document-emailer/swift/lib/swift_required.php'; // Include Swift Mailer
+require_once 'D:\website\webmaster\swift\lib\swift_required.php'; // Include Swift Mailer
+
+// Define result variable
+$result = 0;
+
+// Define the unique token.
+$idToken = uniqid();
 
 // Get variables.
 $textFile = "liz.txt";
@@ -17,13 +23,9 @@ $strDateTime = date("Y-n-j H:i:s", time());
 $link = mysql_connect ("localhost", "user", "pass") or die ("Error: " . mysql_error());
 mysql_select_db("pdfdownload");
 // Perform query.
-$query = "INSERT INTO requests (name, email, ip, request_datetime) VALUES ('" . $strToName . "', '" . $strToEmail . "', '" . $strIP . "', '" . $strDateTime . "')";
-mysql_query($query) or die ("Error updating database.");
-// Close db connection.
-mysql_close ($link);
-
+$queryBeforeSend = mysql_query("INSERT INTO requests (name, email, ip, request_datetime, token) VALUES ('" . $strToName . "', '" . $strToEmail . "', '" . $strIP . "', '" . $strDateTime . "', '" . $idToken . "')") or die ("Error updating database.");
 // Display link.
-echo "Added.<br /><a href='liz.html'>Back</a>";
+echo "Added.<br /><a href='liz.html'>Back</a><br />";
 
 
 // Send email.
@@ -35,16 +37,16 @@ $strSubject = "super awesome pdf attached!"; // edit this to your specified subj
 $strMessage = "Here is the file you requested."; // edit this to you specified message text
 $fileattname = "lizpdf.pdf"; // edit this to your specified filename
 
-//Create the transport with your SMTP settings (Gmail here as example)
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
-  ->setUsername('user@gmail.com')
+// Create the transport with your SMTP settings.
+$transport = Swift_SmtpTransport::newInstance('host', 25)
+  ->setUsername('user')
   ->setPassword('pass')
   ;
 
-//Create the mailer using your created transport
+// Create the mailer using your created transport.
 $mailer = Swift_Mailer::newInstance($transport);
 
-//Create the message
+// Create the message.
 $message = Swift_Message::newInstance()
 	//Give the message a subject
 	->setSubject($strSubject)
@@ -58,7 +60,35 @@ $message = Swift_Message::newInstance()
   	->attach(Swift_Attachment::fromPath($fileattname))
 	;
 
-//Send the message
+// Send the message.
 $result = $mailer->send($message);
+
+// Check for success.
+$queryAfterSend = mysql_query("SELECT id FROM requests WHERE token = '" . $idToken . "'") or die ("Error retrieving token.");
+$row = mysql_fetch_row($queryAfterSend);
+echo $row[0];
+
+if ($result == 0)
+{
+	printf("Oh, crap! Sent %d messages.\n", $result);
+	echo $row[0];
+	$strEmailDateTime = date("Y-n-j H:i:s", time());
+	$queryInsertConfirmationNo = "UPDATE requests SET email_datetime = '" . $strEmailDateTime . "', email_success = 0 WHERE id = ". $row[0] . "";
+	mysql_query($queryInsertConfirmationNo) or die ("Could not insert confirmation.");
+}
+else
+{
+	printf("Yay! Sent %d messages.\n", $result);
+	echo $row[0];
+	$strEmailDateTime = date("Y-n-j H:i:s", time());
+	$queryInsertConfirmationYes = "UPDATE requests SET email_datetime = '" . $strEmailDateTime . "', email_success = 1 WHERE id = ". $row[0] . "";
+	mysql_query($queryInsertConfirmationYes) or die ("Could not insert confirmation.");
+	echo mysql_error($link);
+}
+
+echo $idToken;
+
+// Close db connection.
+mysql_close ($link);
 
 ?>
--- pdfdownload.sql
+++ pdfdownload.sql
@@ -28,20 +28,12 @@ CREATE TABLE `requests` (
   `email` varchar(255) NOT NULL,
   `ip` varchar(45) NOT NULL,
   `request_datetime` datetime NOT NULL,
-  `token` int(11) DEFAULT NULL,
+  `token` varchar(25) DEFAULT NULL,
+  `email_datetime` datetime DEFAULT NULL,
+  `email_success` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `requests`
---
-
-LOCK TABLES `requests` WRITE;
-/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
-INSERT INTO `requests` VALUES (1,'Jeremy Coulson','jcoulson@co.frederick.va.us','10.18.230.231','2011-04-27 12:49:28',NULL),(2,'Jeremy Coulson','jcoulson@co.frederick.va.us','10.18.230.231','2011-04-27 12:50:44',NULL),(3,'Jeremy Coulson','jcoulson@co.frederick.va.us','10.18.230.231','2011-04-27 12:51:16',NULL),(4,'Jeremy Coulson','jcoulson@co.frederick.va.us','10.18.230.231','2011-04-27 14:40:09',NULL),(5,'Jeremy Coulson','jcoulson@co.frederick.va.us','10.18.230.231','2011-04-27 14:42:22',NULL);
-/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-04-28 12:43:27
+-- Dump completed on 2011-05-13 15:32:33
